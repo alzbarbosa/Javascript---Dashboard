4438f2f266c6dc315bd65e72e234ec3a5148d887
alzbarbosa <alzbarbosa@gmail.com>
2022-05-28 16:12:35 -0700

    picture by
    adjust the author

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
     .then(res => res.json())
     .then(data => {
         document.body.style.backgroundImage = `url(${data.urls.regular})`
-		document.getElementById("author").textContent = `By: ${data.user.name}`
+		document.getElementById("author").textContent = `Picture by: ${data.user.name}`
     })
     .catch(err => {
         // Use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
-		document.getElementById("author").textContent = `By: Dodi Achmad`
+		document.getElementById("author").textContent = `Picture by: Dodi Achmad`
     })
 }
 
